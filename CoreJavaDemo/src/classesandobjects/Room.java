e1a1393eb79d9319a3bd7ce1ace8783c97eb2fdb
smei8 <smei821@gmail.com>
2022-02-06 15:26:31 -0500

    constructors

--- CoreJavaDemo/src/classesandobjects/Room.java
+++ CoreJavaDemo/src/classesandobjects/Room.java
@@ -1,10 +1,35 @@
 package classesandobjects;
 
 public class Room {
-	int breadth;
+	//when instance var does not have public specifier, it cannot be access in another class that is in another package
+	//these two instance var is different then the one from the constructor
 	int length;
-	int height;
+	int breadth;
+	static int height;
 	
+	int roomNum;
+	String roomColor;
+	
+	static {
+		height = 50;
+	}
+	public Room(int length, int breadth) { // this is a default constructor because there is no access specifier in the front
+		//this length and breadth is local to this constructor method
+		//this keyword refers to the current object, the instance variable
+		//use this keyword only when the variable name are the same otherwise is not needed
+		this.length = length;
+		this.breadth = breadth;
+	}
+	
+	//constructor overloading - a type of polymorphism
+	public Room(int length, int breadth, int roomNum, String roomColor) {
+//		this.length = length;
+//		this.breadth = breadth;
+		//constructor chaining
+		this(length, breadth);
+		this.roomNum = roomNum;
+		this.roomColor = roomColor;
+	}
 	int calculateFloorArea() {
 		int floorArea = length * breadth;
 		return floorArea;
--- CoreJavaDemo/src/classesandobjects/RoomDemo.java
+++ CoreJavaDemo/src/classesandobjects/RoomDemo.java
@@ -5,23 +5,27 @@ public class RoomDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Room room1 = new Room();
+//		Room room1 = new Room();
+//		room1.length = 16;
+//		room1.breadth = 13;
+		//room1.height = 10;
 		
-		room1.length = 16;
-		room1.breadth = 13;
-		room1.height = 10;
+		Room room1 = new Room(200, 100);
 		
 		System.out.println("Floor area: " + room1.calculateFloorArea());
 		System.out.println("Painting cost:  " + room1.costOfPainting(2));
 		
-		Room room2 = new Room();
+//		Room room2 = new Room();
+//		room2.length = 20;
+//		room2.breadth = 34;
+		//room2.height = 12;
 		
-		room2.length = 20;
-		room2.breadth = 34;
-		room2.height = 12;
+		Room room2 = new Room(300, 150);
 		
-		System.out.println("Floor area: " + room1.calculateFloorArea());
-		System.out.println("Painting cost:  " + room1.costOfPainting(3));
+		System.out.println("Floor area: " + room2.calculateFloorArea());
+		System.out.println("Painting cost:  " + room2.costOfPainting(3));
+		
+		Room room3 = new Room(400, 200, 101, "red");
 		
 	}
 
