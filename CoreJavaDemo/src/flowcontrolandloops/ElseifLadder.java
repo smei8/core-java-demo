cf27d46e481631ff8bca53e098f925ed23efcfa4
smei8 <smei821@gmail.com>
2022-02-05 19:21:23 -0500

    my verison

---
+++ CoreJavaDemo/src/flowcontrolandloops/ElseifLadder.java
@@ -0,0 +1,27 @@
+package flowcontrolandloops;
+
+import java.util.Scanner;
+
+public class ElseifLadder {
+
+	public static void main(String[] args) {
+		// check if a number is less than 0 or b/w 1 and 100 or greater than 100
+		
+		//input
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		int number = scan.nextInt();
+
+		String output = " ";
+		if (number < 0) {
+			output = number + " is less than 0";
+		} else if (number >= 1 && number <= 100 ) {
+			output = number + " is in between 1 and 100";
+		} else if (number > 100) {
+			output = number + " is greater than 100";
+		}
+		
+		System.out.println(output);
+	}
+
+}
